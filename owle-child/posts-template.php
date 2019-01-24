d388386553b9ae60802d82a7e3f7bc8127919a32
unknown <miket@meta.ua>
2019-01-24 19:13:16 +0200

    end of day

--- owle-child/posts-template.php
+++ owle-child/posts-template.php
@@ -10,23 +10,10 @@
                 <div class="news">
                     <div class="news-top-line">
                         <?php if ( is_active_sidebar( 'home_right_1' ) ) : ?>
-                            <div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+                            <div class="tags-wrapper">
                                 <?php dynamic_sidebar( 'home_right_1' ); ?>
                             </div> <!-- #primary-sidebar -->
                         <?php endif; ?>
-<!--                        <div class="tags-wrapper">-->
-<!--                            <div class="tags-block">-->
-<!--                                <div class="ant-tag">tag</div>-->
-<!--                                <div class="ant-tag">Some text</div>-->
-<!--                                <div class="ant-tag">tag</div>-->
-<!--                                <div class="ant-tag">tag</div>-->
-<!--                                <div class="ant-tag">tag</div>-->
-<!--                                <div class="ant-tag">tag</div>-->
-<!--                            </div>-->
-<!--                            <i class="anticon">-->
-<!--                                <svg viewBox="64 64 896 896" class="" data-icon="setting" width="20px" height="20px" fill="currentColor" aria-hidden="true"><path d="M924.8 625.7l-65.5-56c3.1-19 4.7-38.4 4.7-57.8s-1.6-38.8-4.7-57.8l65.5-56a32.03 32.03 0 0 0 9.3-35.2l-.9-2.6a443.74 443.74 0 0 0-79.7-137.9l-1.8-2.1a32.12 32.12 0 0 0-35.1-9.5l-81.3 28.9c-30-24.6-63.5-44-99.7-57.6l-15.7-85a32.05 32.05 0 0 0-25.8-25.7l-2.7-.5c-52.1-9.4-106.9-9.4-159 0l-2.7.5a32.05 32.05 0 0 0-25.8 25.7l-15.8 85.4a351.86 351.86 0 0 0-99 57.4l-81.9-29.1a32 32 0 0 0-35.1 9.5l-1.8 2.1a446.02 446.02 0 0 0-79.7 137.9l-.9 2.6c-4.5 12.5-.8 26.5 9.3 35.2l66.3 56.6c-3.1 18.8-4.6 38-4.6 57.1 0 19.2 1.5 38.4 4.6 57.1L99 625.5a32.03 32.03 0 0 0-9.3 35.2l.9 2.6c18.1 50.4 44.9 96.9 79.7 137.9l1.8 2.1a32.12 32.12 0 0 0 35.1 9.5l81.9-29.1c29.8 24.5 63.1 43.9 99 57.4l15.8 85.4a32.05 32.05 0 0 0 25.8 25.7l2.7.5a449.4 449.4 0 0 0 159 0l2.7-.5a32.05 32.05 0 0 0 25.8-25.7l15.7-85a350 350 0 0 0 99.7-57.6l81.3 28.9a32 32 0 0 0 35.1-9.5l1.8-2.1c34.8-41.1 61.6-87.5 79.7-137.9l.9-2.6c4.5-12.3.8-26.3-9.3-35zM788.3 465.9c2.5 15.1 3.8 30.6 3.8 46.1s-1.3 31-3.8 46.1l-6.6 40.1 74.7 63.9a370.03 370.03 0 0 1-42.6 73.6L721 702.8l-31.4 25.8c-23.9 19.6-50.5 35-79.3 45.8l-38.1 14.3-17.9 97a377.5 377.5 0 0 1-85 0l-17.9-97.2-37.8-14.5c-28.5-10.8-55-26.2-78.7-45.7l-31.4-25.9-93.4 33.2c-17-22.9-31.2-47.6-42.6-73.6l75.5-64.5-6.5-40c-2.4-14.9-3.7-30.3-3.7-45.5 0-15.3 1.2-30.6 3.7-45.5l6.5-40-75.5-64.5c11.3-26.1 25.6-50.7 42.6-73.6l93.4 33.2 31.4-25.9c23.7-19.5 50.2-34.9 78.7-45.7l37.9-14.3 17.9-97.2c28.1-3.2 56.8-3.2 85 0l17.9 97 38.1 14.3c28.7 10.8 55.4 26.2 79.3 45.8l31.4 25.8 92.8-32.9c17 22.9 31.2 47.6 42.6 73.6L781.8 426l6.5 39.9zM512 326c-97.2 0-176 78.8-176 176s78.8 176 176 176 176-78.8 176-176-78.8-176-176-176zm79.2 255.2A111.6 111.6 0 0 1 512 614c-29.9 0-58-11.7-79.2-32.8A111.6 111.6 0 0 1 400 502c0-29.9 11.7-58 32.8-79.2C454 401.6 482.1 390 512 390c29.9 0 58 11.6 79.2 32.8A111.6 111.6 0 0 1 624 502c0 29.9-11.7 58-32.8 79.2z"></path></svg>-->
-<!--                            </i>-->
-<!--                        </div>-->
                         <div class="news-search">
                             <label class="ant-checkbox-wrapper">
                                 <span class="ant-checkbox ant-checkbox-checked">
@@ -47,7 +34,96 @@
                             </span>
                         </div>
                     </div>
-                    <div class="news-wrapper"></div>
+                    <div class="news-wrapper">
+                        <div class="news-top">
+                            <?php
+                                $first_query = new WP_Query('post_type=post&post_status=publish&posts_per_page=1');
+
+                                while ( $first_query->have_posts() ) : $first_query->the_post();
+                            ?>
+                                    <div class="news-card">
+                                        <?php the_shortlink(' ', null, '', ''); ?>
+                                        <div class="news-head">
+                                            <div class="news-title"><?php the_title(); ?></div>
+                                            <div class="views-counter">
+                                                <i class="icon-eye">
+                                                    <svg viewBox="64 64 896 896" class="" data-icon="eye" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M942.2 486.2C847.4 286.5 704.1 186 512 186c-192.2 0-335.4 100.5-430.2 300.3a60.3 60.3 0 0 0 0 51.5C176.6 737.5 319.9 838 512 838c192.2 0 335.4-100.5 430.2-300.3 7.7-16.2 7.7-35 0-51.5zM512 766c-161.3 0-279.4-81.8-362.7-254C232.6 339.8 350.7 258 512 258c161.3 0 279.4 81.8 362.7 254C791.5 684.2 673.4 766 512 766zm-4-430c-97.2 0-176 78.8-176 176s78.8 176 176 176 176-78.8 176-176-78.8-176-176-176zm0 288c-61.9 0-112-50.1-112-112s50.1-112 112-112 112 50.1 112 112-50.1 112-112 112z"></path></svg>
+                                                </i>
+                                                <span><?php echo substr(the_views(), 0, strpos(the_views(), ' ')); ?></span>
+                                            </div>
+                                        </div>
+                                        <div class="news-about">
+                                            <div class="news-category"><?php foreach((get_the_category()) as $category) { echo $category->cat_name; } ?></div>
+                                            <div class="news-date"><?php the_time("d.m.Y");  ?></div>
+                                        </div>
+                                        <div class="news-body">
+                                            <?php the_excerpt(); ?>
+                                        </div>
+                                    </div>
+                            <?php
+                                endwhile;
+
+                                wp_reset_postdata();
+                            ?>
+                            <div class="rightside-news">
+                                <?php
+                                    $second_query = new WP_Query('post_type=post&post_status=publish&posts_per_page=2&offset=1');
+
+                                    while ( $second_query->have_posts() ) : $second_query->the_post();
+                                ?>
+                                    <div class="news-card">
+                                        <?php the_shortlink(' ', null, '', ''); ?>
+                                        <div class="news-head">
+                                            <div class="news-title"><?php the_title(); ?></div>
+                                            <div class="views-counter">
+                                                <i class="icon-eye">
+                                                    <svg viewBox="64 64 896 896" class="" data-icon="eye" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M942.2 486.2C847.4 286.5 704.1 186 512 186c-192.2 0-335.4 100.5-430.2 300.3a60.3 60.3 0 0 0 0 51.5C176.6 737.5 319.9 838 512 838c192.2 0 335.4-100.5 430.2-300.3 7.7-16.2 7.7-35 0-51.5zM512 766c-161.3 0-279.4-81.8-362.7-254C232.6 339.8 350.7 258 512 258c161.3 0 279.4 81.8 362.7 254C791.5 684.2 673.4 766 512 766zm-4-430c-97.2 0-176 78.8-176 176s78.8 176 176 176 176-78.8 176-176-78.8-176-176-176zm0 288c-61.9 0-112-50.1-112-112s50.1-112 112-112 112 50.1 112 112-50.1 112-112 112z"></path></svg>
+                                                </i>
+                                                <span><?php echo substr(the_views(), 0, strpos(the_views(), ' ')); ?></span>
+                                            </div>
+                                        </div>
+                                        <div class="news-about">
+                                            <div class="news-category"><?php foreach((get_the_category()) as $category) { echo $category->cat_name; } ?></div>
+                                            <div class="news-date"><?php the_time("d.m.Y");  ?></div>
+                                        </div>
+                                    </div>
+                                <?php
+                                    endwhile;
+
+                                    wp_reset_postdata();
+                                ?>
+                            </div>
+                        </div>
+                        <div class="news-block">
+                            <?php
+                                $third_query = new WP_Query('post_type=post&post_status=publish&post_per_page=-1&offset=3');
+
+                                while ( $third_query->have_posts() ) : $third_query->the_post(); ?>
+                                    <div class="news-card">
+                                        <?php the_shortlink(' ', null, '', ''); ?>
+                                        <div class="news-head">
+                                            <div class="news-title"><?php the_title(); ?></div>
+                                            <div class="views-counter">
+                                                <i class="icon-eye">
+                                                    <svg viewBox="64 64 896 896" class="" data-icon="eye" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M942.2 486.2C847.4 286.5 704.1 186 512 186c-192.2 0-335.4 100.5-430.2 300.3a60.3 60.3 0 0 0 0 51.5C176.6 737.5 319.9 838 512 838c192.2 0 335.4-100.5 430.2-300.3 7.7-16.2 7.7-35 0-51.5zM512 766c-161.3 0-279.4-81.8-362.7-254C232.6 339.8 350.7 258 512 258c161.3 0 279.4 81.8 362.7 254C791.5 684.2 673.4 766 512 766zm-4-430c-97.2 0-176 78.8-176 176s78.8 176 176 176 176-78.8 176-176-78.8-176-176-176zm0 288c-61.9 0-112-50.1-112-112s50.1-112 112-112 112 50.1 112 112-50.1 112-112 112z"></path></svg>
+                                                </i>
+                                                <span><?php echo substr(the_views(), 0, strpos(the_views(), ' ')); ?></span>
+                                            </div>
+                                        </div>
+                                        <div class="news-about">
+                                            <div class="news-category"><?php foreach((get_the_category()) as $category) { echo $category->cat_name; } ?></div>
+                                            <div class="news-date"><?php the_time("d.m.Y");  ?></div>
+                                        </div>
+                                        <div class="news-body">
+                                            <?php the_excerpt(); ?>
+                                        </div>
+                                    </div>
+                                <?php endwhile;
+
+                                wp_reset_postdata();
+                            ?>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
